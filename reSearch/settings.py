ade80d41351c9cbe7ecd72b4ff9b8b9001133b0f
chiraag-kakar <chirucos1998@gmail.com>
2021-07-05 16:21:51 +0530

    installed cors app

--- reSearch/settings.py
+++ reSearch/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'research_api',
+    'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
